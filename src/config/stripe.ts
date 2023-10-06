f65416748cf20ceb880a9d8b667906cd9d8814b4
joshua maeba <joshuamaeba@gmail.com>
2023-10-06 12:57:32 +0300

    initial commit

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 2500,
+      priceIds: {
+        test: "price_1NxoH8IPJNMhLE5LHOlVl6CW",
+        production: "",
+      },
+    },
+  },
+];
